ce1f8b8fa5951b633c975943a2e7ad230be50bf0
ANUKALP <ANUKALP@LAPTOP-MJJABJ1L>
2020-03-17 23:42:30 +0530

    //3rd Attempt

--- TestProject/src/ClsTest.java
+++ TestProject/src/ClsTest.java
@@ -3,7 +3,11 @@ public class ClsTest {
 
 	public static void main(String[] args) {
 		System.out.println("first github test");
-
+		ClsTest ct=new ClsTest();
+		ct.add(10, 20);
+	}
+	public int add(int x,int y) {
+		return x+y;
 	}
 
 }
